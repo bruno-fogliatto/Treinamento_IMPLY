d9ecf91c46da188d31de6f88e16694367077e83a
bruno <blopes@imply.com>
2023-04-11 11:41:26 -0300

    Atualização do codigo de fatoracao recursivo

--- Desafio 01/Fatoracao.php
+++ Desafio 01/Fatoracao.php
@@ -1,2 +1,18 @@
 <?php
-var_dump('hello');
+    $num = 10;
+
+
+    $fator = fatorar($num);
+
+    var_dump($fator);
+
+
+    function fatorar ($num){
+        if($num < 0){
+            return -1;
+        }else if($num == 0){
+            return 1;
+        }else{
+            return ($num * fatorar($num -1));
+        }
+    }
