f7dbf62797100da27dded99c1da4e295a65d8223
bruno <blopes@imply.com>
2023-04-11 18:21:05 -0300

    Desafios 02

---
+++ Desafio 02/ValidacaoEmail.php
@@ -0,0 +1,9 @@
+<?php
+    $mail = readline('Informe seu e-mail: ');
+
+    function validar($mail){
+        if(!empty($mail)){
+            $regexp = "^[a-zA-Z0-9\._-]+@+[a-zA-Z0-9\._-]+.";
+
+        }
+    }
