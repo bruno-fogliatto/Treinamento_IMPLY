60458f00bb07de15675eba1ec2ec3e98a0060f65
bruno <blopes@imply.com>
2023-04-12 08:10:44 -0300

    Concusão desafio Validação de e-mail com REGEXP

--- Desafio 02/ValidacaoEmail.php
+++ Desafio 02/ValidacaoEmail.php
@@ -1,9 +1,21 @@
 <?php
     $mail = readline('Informe seu e-mail: ');
 
+    if(validar($mail)){
+        echo "E-mail válido! \n";
+    }else{
+        echo "E-mail inválido! \n";
+    }
+
     function validar($mail){
         if(!empty($mail)){
-            $regexp = "^[a-zA-Z0-9\._-]+@+[a-zA-Z0-9\._-]+.";
+            $regexp = "/^([a-zA-Z0-9\.]+@+[a-zA-Z]+(\.)+[a-zA-Z]{2,3})$/";
 
+            if(preg_match($regexp, $mail)){
+                return true;
+            }else{
+                return false;
+            }
         }
+        return false;
     }
